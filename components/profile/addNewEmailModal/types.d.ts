8eeb7b9a6539e90682ccb2444407231955a98b17
datomaluta <datomaluta@gmail.com>
2023-02-12 13:00:59 +0400

    fix: use reusable generic type for setter

--- components/profile/addNewEmailModal/types.d.ts
+++ components/profile/addNewEmailModal/types.d.ts
@@ -1,9 +1,11 @@
 import { SetState } from 'types'
 
-export type PropsType = { showOrHideEmailModal: (boolean) => void }
+export type PropsType = { showOrHideEmailModal: SetState<boolean> }
+
 export type HookPropsType = {
   showOrHideModal: SetState<boolean>
 }
+
 export type DataType = {
   email: string
 }
